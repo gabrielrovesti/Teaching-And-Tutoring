76d6ff6d79a3b35bd2b1b4281969446a24e41b00
Gabriel Rovesti <rovestigabriel@gmail.com>
2023-08-15 11:25:48 +0200

    Preparazione di tutte le lezioni di P2

---
+++ Freelance/Programmazione ad oggetti/22-08/Es1_Soluzione.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+class A{
+    public:
+        virtual ~A() = default;
+};
+
+class D: public A {};
+
+class C: public A{};
+
+class B: public D {};
+
+template <class X, class Y>
+X* fun(X* p) { return dynamic_cast<Y*>(p);}
+
+int main(){
+    C c; fun<A,B>(&c);
+    if(fun<A,B>(new C())==0) cout << "Bjarne ";
+    if(dynamic_cast<C*>(new B()) == 0) cout << "Stroustrop";
+    A* p = fun<D, B>(new D());
+}
+
+/*
+A <= B - Possibile
+A <= C - Falso
+A <= D - Falso
+B <= A - Vero
+B <= C - Falso
+B <= D - Vero
+C <= A - Vero
+C <= B - Falso
+C <= D - Possibile
+D <= A - Vero
+D <= B - Vero
+D <= C - Possibile
+*/
