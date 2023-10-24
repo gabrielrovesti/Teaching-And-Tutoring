0d47c65d7ec412415b4bb1b4ec410d5971e26dc0
Gabriel Rovesti <rovestigabriel@gmail.com>
2023-10-24 17:06:34 +0200

    Registro ore, materiale tutorato domani, organizzazione

--- UniPD/P2 - Semestre 1/25-10/Esercizi/Es3.cpp
+++ UniPD/P2 - Semestre 1/25-10/Esercizi/Es3.cpp
@@ -1,15 +1,32 @@
-// COSA STAMPA IL SEGUENTE PROGRAMMA: ASSEGNAZIONE DI DEFAULT C++
+// Cosa stampa?
+
 #include <iostream>
 using namespace std;
-class Puntatore {
-public:
-    int* punt;
+
+class D {
+    private:
+        int z;
+    public:
+        D(int x=0): z(x) { cout << "D01 "; }
+        D(const D& a): z(a.z) { cout << "Dc "; }
 };
+
+class C {
+    private:
+        D d;
+    public:
+        C() : d(D(5)) { cout << "C0 ";}
+        C(int a) : d(5) { cout << "C1 ";}
+        C(const C& c) : d(c.d) { cout << "Cc ";}
+};
+
 int main() {
-    Puntatore x, y;
-    x.punt = new int(8);
-    y = x;
-    cout << "*(y.punt) = " << *(y.punt) << endl;
-    *(y.punt) = 3;
-    cout << "*(x.punt) = " << *(x.punt) << endl;
+    C c1;
+    cout << "UNO" << endl;
+    C c2(3);
+    cout << "DUE" << endl;
+    C c3(c2);
+    cout << "TRE" << endl;
+
+return 0;
 }
--- UniPD/P2 - Semestre 1/25-10/Soluzioni/Es3_Soluzione.cpp
+++ UniPD/P2 - Semestre 1/25-10/Soluzioni/Es3_Soluzione.cpp
@@ -1,15 +1,32 @@
-// COSA STAMPA IL SEGUENTE PROGRAMMA: ASSEGNAZIONE DI DEFAULT C++
+// Cosa stampa?
+
 #include <iostream>
 using namespace std;
-class Puntatore {
-public:
-    int* punt; // CLASSE PUNT A INT, NO COSTRUTTORI, NO OPERATORI ASS. (METODI DEFUALT ES. COSTRUTTORE DEFUALT)
+
+class D {
+    private:
+        int z;
+    public:
+        D(int x=0): z(x) { cout << "D01 "; }
+        D(const D& a): z(a.z) { cout << "Dc "; }
 };
+
+class C {
+    private:
+        D d;
+    public:
+        C() : d(D(5)) { cout << "C0 ";}
+        C(int a) : d(5) { cout << "C1 ";}
+        C(const C& c) : d(c.d) { cout << "Cc ";}
+};
+
 int main() {
-Puntatore x, y;
-x.punt = new int(8);
-y = x;
-cout << "*(y.punt) = " << *(y.punt) << endl;
-*(y.punt) = 3; // DATO CHE Y.PUNT PUNTA ALLA STESSA AREA DI MEM X.PUNT, ENTRAMBI AVRANNO LO STESSO VALORE
-cout << "*(x.punt) = " << *(x.punt) << endl;
+    C c1; // 1- CHIAMATO COSTRUTTORE DEFAULT (R.19) 2- CHIAMA COSTRUTTORE D (R.11)
+    cout << "UNO" << endl; //D01 Dc C0 UNO -- POTREBBE NON STAMPARE Dc PER UN DISCORSO DI OTTIMIZZAZIONE DEI COSTRUTTORI DI COPIA
+    C c2(3); // 1- CHIAMA COSTRTUTTORE C (R.20) 2- CHIAMA COSTRUTTORE D(R.11)
+    cout << "DUE" << endl; // D01 C1 DUE
+    C c3(c2); // 1- CHIAMA COSTRUTTORE COPIA C (R.21) 2- CHIAMA COSTRUTTORE COPIA D (R. 12)
+    cout << "TRE" << endl; // Dc Cc TRE
+
+return 0;
 }
