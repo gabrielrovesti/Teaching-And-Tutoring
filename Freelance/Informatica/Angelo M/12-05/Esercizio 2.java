8626b5fc23d18ea1220717110cfe575cb7230c0e
Gabriel Rovesti <rovestigabriel@gmail.com>
2024-05-13 11:36:22 +0200

    Lavoro di ieri

--- Freelance/Informatica/Angelo M/12-05/Esercizio 2.java
+++ Freelance/Informatica/Angelo M/12-05/Esercizio 2.java
@@ -1,216 +1,71 @@
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
+public class Person {
+    private String name;
+    private int birthYear;
 
-// Classe base Employee
-abstract class Employee {
-    protected String firstName;
-    protected String lastName;
-    protected LocalDate hireDate;
-
-    public Employee(String firstName, String lastName, LocalDate hireDate) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.hireDate = hireDate;
+    public Person(String name, int birthYear) {
+        this.name = name;
+        this.birthYear = birthYear;
     }
 
-    public abstract double calculatePay();
-
-    public String getFullName() {
-        return firstName + " " + lastName;
-    }
-}
-
-// Dipendente a tempo pieno
-class FullTimeEmployee extends Employee {
-    private double annualSalary;
-    private double annualBonus;
-
-    public FullTimeEmployee(String firstName, String lastName, LocalDate hireDate, double annualSalary) {
-        super(firstName, lastName, hireDate);
-        this.annualSalary = annualSalary;
+    public String getName() {
+        return name;
     }
 
-    public void setAnnualBonus(double annualBonus) {
-        this.annualBonus = annualBonus;
-    }
-
-    @Override
-    public double calculatePay() {
-        return (annualSalary + annualBonus) / 12;
+    public int getBirthYear() {
+        return birthYear;
     }
 
     @Override
     public String toString() {
-        return "Dipendente a tempo pieno: " + getFullName() + ", Stipendio annuale: " + annualSalary + ", Bonus annuale: " + annualBonus;
+        return "Nome: " + name + ", Anno di nascita: " + birthYear;
     }
 }
 
-// Dipendente part-time
-class PartTimeEmployee extends Employee {
-    private double hourlyRate;
-    private int weeklyHours;
-    private double overtimeRate;
+public class Student extends Person {
+    private String major;
 
-    public PartTimeEmployee(String firstName, String lastName, LocalDate hireDate, double hourlyRate, double overtimeRate) {
-        super(firstName, lastName, hireDate);
-        this.hourlyRate = hourlyRate;
-        this.overtimeRate = overtimeRate;
+    public Student(String name, int birthYear, String major) {
+        super(name, birthYear);
+        this.major = major;
     }
 
-    public void setWeeklyHours(int weeklyHours) {
-        this.weeklyHours = weeklyHours;
-    }
-
-    @Override
-    public double calculatePay() {
-        int regularHours = Math.min(weeklyHours, 40);
-        int overtimeHours = Math.max(weeklyHours - 40, 0);
-        double regularPay = regularHours * hourlyRate;
-        double overtimePay = overtimeHours * hourlyRate * overtimeRate;
-        return regularPay + overtimePay;
+    public String getMajor() {
+        return major;
     }
 
     @Override
     public String toString() {
-        return "Dipendente part-time: " + getFullName() + ", Paga oraria: " + hourlyRate + ", Ore settimanali: " + weeklyHours + ", Maggiorazione straordinario: " + overtimeRate;
+        return super.toString() + ", Disciplina di specializzazione: " + major;
     }
 }
 
-// Dirigente
-class Manager extends FullTimeEmployee {
-    private double companyPerformanceBonus;
-    private String companyCarModel;
-    private double stockOptionValue;
+public class Instructor extends Person {
+    private double salary;
 
-    public Manager(String firstName, String lastName, LocalDate hireDate, double annualSalary, double companyPerformanceBonus, String companyCarModel, double stockOptionValue) {
-        super(firstName, lastName, hireDate, annualSalary);
-        this.companyPerformanceBonus = companyPerformanceBonus;
-        this.companyCarModel = companyCarModel;
-        this.stockOptionValue = stockOptionValue;
+    public Instructor(String name, int birthYear, double salary) {
+        super(name, birthYear);
+        this.salary = salary;
     }
 
-    @Override
-    public double calculatePay() {
-        return super.calculatePay() + companyPerformanceBonus / 12;
+    public double getSalary() {
+        return salary;
     }
 
     @Override
     public String toString() {
-        return "Dirigente: " + getFullName() + ", " + super.toString() + ", Bonus performance aziendale: " + companyPerformanceBonus + ", Auto aziendale: " + companyCarModel + ", Valore stock option: " + stockOptionValue;
+        return super.toString() + ", Salario: " + salary;
     }
 }
 
-// Consulente esterno
-class Contractor extends Employee {
-    private double dailyRate;
-    private int workingDays;
-    private int maxWorkingDays;
-
-    public Contractor(String firstName, String lastName, LocalDate hireDate, double dailyRate, int maxWorkingDays) {
-        super(firstName, lastName, hireDate);
-        this.dailyRate = dailyRate;
-        this.maxWorkingDays = maxWorkingDays;
-    }
-
-    public void setWorkingDays(int workingDays) {
-        this.workingDays = Math.min(workingDays, maxWorkingDays);
-    }
-
-    @Override
-    public double calculatePay() {
-        return dailyRate * workingDays;
-    }
-
-    @Override
-    public String toString() {
-        return "Consulente esterno: " + getFullName() + ", Tariffa giornaliera: " + dailyRate + ", Giorni lavorativi: " + workingDays + ", Limite massimo giorni: " + maxWorkingDays;
-    }
-}
-
-// Stagista
-class Intern extends Employee {
-    private double monthlyStipend;
-    private int internshipDuration; // In mesi
-
-    public Intern(String firstName, String lastName, LocalDate hireDate, double monthlyStipend, int internshipDuration) {
-        super(firstName, lastName, hireDate);
-        this.monthlyStipend = monthlyStipend;
-        this.internshipDuration = internshipDuration;
-    }
-
-    @Override
-    public double calculatePay() {
-        return monthlyStipend * internshipDuration;
-    }
-
-    @Override
-    public String toString() {
-        return "Stagista: " + getFullName() + ", Indennità mensile: " + monthlyStipend + ", Durata stage: " + internshipDuration + " mesi";
-    }
-}
-
-// Classe principale per la gestione delle risorse umane
-class HumanResourcesSystem {
-    private List<Employee> employees;
-
-    public HumanResourcesSystem() {
-        employees = new ArrayList<>();
-    }
-
-    public void addEmployee(Employee employee) {
-        employees.add(employee);
-    }
-
-    public void removeEmployee(Employee employee) {
-        employees.remove(employee);
-    }
-
-    public double calculateTotalPayroll() {
-        double totalPayroll = 0;
-        for (Employee employee : employees) {
-            totalPayroll += employee.calculatePay();
-        }
-        return totalPayroll;
-    }
-
-    public void displayEmployeeInfo() {
-        for (Employee employee : employees) {
-            System.out.println(employee);
-        }
-    }
-}
-
-// Classe principale
-public class Main {
+public class PersonTester {
     public static void main(String[] args) {
-        HumanResourcesSystem hrs = new HumanResourcesSystem();
+        Person person1 = new Person("John Doe", 1990);
+        System.out.println(person1);
 
-        // Aggiunta di dipendenti
-        FullTimeEmployee fte1 = new FullTimeEmployee("John", "Doe", LocalDate.of(2020, 1, 1), 60000);
-        fte1.setAnnualBonus(5000);
-        hrs.addEmployee(fte1);
+        Student student1 = new Student("Jane Smith", 1995, "Computer Science");
+        System.out.println(student1);
 
-        PartTimeEmployee pte1 = new PartTimeEmployee("Jane", "Smith", LocalDate.of(2022, 3, 15), 20, 1.5);
-        pte1.setWeeklyHours(30);
-        hrs.addEmployee(pte1);
-
-        Manager manager1 = new Manager("Bob", "Johnson", LocalDate.of(2018, 6, 1), 100000, 20000, "BMW X5", 50000);
-        hrs.addEmployee(manager1);
-
-        Contractor contractor1 = new Contractor("Alice", "Williams", LocalDate.of(2023, 2, 1), 500, 200);
-        contractor1.setWorkingDays(150);
-        hrs.addEmployee(contractor1);
-
-        Intern intern1 = new Intern("Michael", "Brown", LocalDate.of(2023, 5, 1), 1000, 6);
-        hrs.addEmployee(intern1);
-
-    // Visualizzazione delle informazioni dei dipendenti
-    System.out.println("\nInformazioni dei dipendenti:");
-    hrs.displayEmployeeInfo();
-
-    // Calcolo della spesa salariale totale
-    double totalPayroll = hrs.calculateTotalPayroll();
-    System.out.println("\nSpesa salariale totale: $" + totalPayroll);
-    }  
+        Instructor instructor1 = new Instructor("Robert Johnson", 1980, 75000.0);
+        System.out.println(instructor1);
+    }
 }
--- Freelance/Informatica/Angelo M/12-05/Esercizio 3.java
+++ Freelance/Informatica/Angelo M/12-05/Esercizio 3.java
@@ -1,71 +1,260 @@
-public class Person {
-    private String name;
-    private int birthYear;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.List;
 
-    public Person(String name, int birthYear) {
-        this.name = name;
-        this.birthYear = birthYear;
+abstract class Persona {
+    private String nome;
+    private String cognome;
+    private LocalDate dataNascita;
+    private String residenza;
+
+    public Persona(String nome, String cognome, LocalDate dataNascita, String residenza) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.dataNascita = dataNascita;
+        this.residenza = residenza;
     }
 
-    public String getName() {
-        return name;
+    public String getNome() {
+        return nome;
     }
 
-    public int getBirthYear() {
-        return birthYear;
+    public String getCognome() {
+        return cognome;
+    }
+
+    public LocalDate getDataNascita() {
+        return dataNascita;
+    }
+
+    public String getResidenza() {
+        return residenza;
+    }
+
+    public void setResidenza(String residenza) {
+        this.residenza = residenza;
+    }
+
+    public int calcolaEta() {
+        return Period.between(dataNascita, LocalDate.now()).getYears();
+    }
+
+    public boolean isResidenteIn(String citta) {
+        return residenza.equalsIgnoreCase(citta);
+    }
+
+    public boolean haLoStessoCognome(Persona p) {
+        return cognome.equalsIgnoreCase(p.getCognome());
+    }
+
+    public static int confrontaEta(Persona p1, Persona p2) {
+        return p1.calcolaEta() - p2.calcolaEta();
     }
 
     @Override
     public String toString() {
-        return "Nome: " + name + ", Anno di nascita: " + birthYear;
+        return nome + " " + cognome + ", nato il " + dataNascita + ", residente a " + residenza;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if (other == null || getClass() != other.getClass()) return false;
+        Persona persona = (Persona) other;
+        return nome.equals(persona.nome) && cognome.equals(persona.cognome) && dataNascita.equals(persona.dataNascita) && residenza.equals(persona.residenza);
     }
 }
 
-public class Student extends Person {
-    private String major;
+abstract class StudenteUniversitario extends Persona {
+    private int matricola;
+    private int annoImmatricolazione;
+    private String corsoDiLaurea;
 
-    public Student(String name, int birthYear, String major) {
-        super(name, birthYear);
-        this.major = major;
+    public StudenteUniversitario(String nome, String cognome, LocalDate dataNascita, String residenza, int matricola, int annoImmatricolazione, String corsoDiLaurea) {
+        super(nome, cognome, dataNascita, residenza);
+        this.matricola = matricola;
+        this.annoImmatricolazione = annoImmatricolazione;
+        this.corsoDiLaurea = corsoDiLaurea;
     }
 
-    public String getMajor() {
-        return major;
+    public int getMatricola() {
+        return matricola;
+    }
+
+    public int getAnnoImmatricolazione() {
+        return annoImmatricolazione;
+    }
+
+    public String getCorsoDiLaurea() {
+        return corsoDiLaurea;
+    }
+
+    public void setCorsoDiLaurea(String corsoDiLaurea) {
+        this.corsoDiLaurea = corsoDiLaurea;
+    }
+
+    public int calcolaAnniIscrizione() {
+        return LocalDate.now().getYear() - annoImmatricolazione;
+    }
+
+    public boolean isImmatricolatoInCorso(String corso) {
+        return corsoDiLaurea.equalsIgnoreCase(corso);
+    }
+
+    public boolean isStudenteFuoriCorso() {
+        return calcolaAnniIscrizione() > 5;
+    }
+
+    public static boolean isStudenteLavoratore(Persona p) {
+        return p instanceof StudenteLavoratore;
     }
 
     @Override
     public String toString() {
-        return super.toString() + ", Disciplina di specializzazione: " + major;
+        return super.toString() + ", matricola " + matricola + ", iscritto al corso " + corsoDiLaurea + " dall'anno " + annoImmatricolazione;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if (other == null || getClass() != other.getClass()) return false;
+        if (!super.equals(other)) return false;
+        StudenteUniversitario that = (StudenteUniversitario) other;
+        return matricola == that.matricola && annoImmatricolazione == that.annoImmatricolazione && corsoDiLaurea.equals(that.corsoDiLaurea);
     }
 }
 
-public class Instructor extends Person {
-    private double salary;
-
-    public Instructor(String name, int birthYear, double salary) {
-        super(name, birthYear);
-        this.salary = salary;
+class Studente extends StudenteUniversitario {
+    public Studente(String nome, String cognome, LocalDate dataNascita, String residenza, int matricola, int annoImmatricolazione, String corsoDiLaurea) {
+        super(nome, cognome, dataNascita, residenza, matricola, annoImmatricolazione, corsoDiLaurea);
     }
 
-    public double getSalary() {
-        return salary;
+    public double calcolaTassaIscrizione() {
+        return 1000.0;
+    }
+
+    public boolean isStudenteRegolare() {
+        return calcolaEta() < 25 && !isStudenteFuoriCorso();
+    }
+
+    public static double calcolaTassaMedia(List<Studente> studenti) {
+        double somma = 0;
+        for (Studente s : studenti) {
+            somma += s.calcolaTassaIscrizione();
+        }
+        return somma / studenti.size();
+    }
+}
+
+class StudenteLavoratore extends Studente {
+    private String azienda;
+    private double stipendio;
+
+    public StudenteLavoratore(String nome, String cognome, LocalDate dataNascita, String residenza, int matricola, int annoImmatricolazione, String corsoDiLaurea, String azienda, double stipendio) {
+        super(nome, cognome, dataNascita, residenza, matricola, annoImmatricolazione, corsoDiLaurea);
+        this.azienda = azienda;
+        this.stipendio = stipendio;
+    }
+
+    public String getAzienda() {
+        return azienda;
+    }
+
+    public double getStipendio() {
+        return stipendio;
+    }
+
+    public void setAzienda(String azienda) {
+        this.azienda = azienda;
+    }
+
+    public void setStipendio(double stipendio) {
+        this.stipendio = stipendio;
+    }
+
+    public double calcolaRedditoAnnuale() {
+        return stipendio * 12;
     }
 
     @Override
     public String toString() {
-        return super.toString() + ", Salario: " + salary;
+        return super.toString() + ", lavora presso " + azienda + " con uno stipendio di " + stipendio + " euro al mese";
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if (other == null || getClass() != other.getClass()) return false;
+        if (!super.equals(other)) return false;
+        StudenteLavoratore that = (StudenteLavoratore) other;
+        return azienda.equals(that.azienda) && Double.compare(that.stipendio, stipendio) == 0;
     }
 }
 
-public class PersonTester {
-    public static void main(String[] args) {
-        Person person1 = new Person("John Doe", 1990);
-        System.out.println(person1);
+class StudenteFuoriSede extends Studente {
+    private double affitto;
+    private double borsa;
 
-        Student student1 = new Student("Jane Smith", 1995, "Computer Science");
-        System.out.println(student1);
-
-        Instructor instructor1 = new Instructor("Robert Johnson", 1980, 75000.0);
-        System.out.println(instructor1);
+    public StudenteFuoriSede(String nome, String cognome, LocalDate dataNascita, String residenza, int matricola, int annoImmatricolazione, String corsoDiLaurea, double affitto, double borsa) {
+        super(nome, cognome, dataNascita, residenza, matricola, annoImmatricolazione, corsoDiLaurea);
+        this.affitto = affitto;
+        this.borsa = borsa;
     }
+
+    public double getAffitto() {
+        return affitto;
+    }
+
+    public double getBorsa() {
+        return borsa;
+    }
+
+}
+
+public void setBorsa(double borsa) {
+    this.borsa = borsa;
+}
+
+public double calcolaSpeseAnnuali() {
+    return (affitto * 12) + calcolaTassaIscrizione() - borsa;
+}
+
+@Override
+public String toString() {
+    return super.toString() + ", affitto di " + affitto + " euro al mese" + (borsa > 0 ? ", borsa di studio di " + borsa + " euro" : "");
+}
+
+@Override
+public boolean equals(Object other) {
+    if (this == other) return true;
+    if (other == null || getClass() != other.getClass()) return false;
+    if (!super.equals(other)) return false;
+    StudenteFuoriSede that = (StudenteFuoriSede) other;
+    return Double.compare(that.affitto, affitto) == 0 && Double.compare(that.borsa, borsa) == 0;
+}
+
+public class Main {
+public static void main(String[] args) {
+    Persona p1 = new Persona("Mario", "Rossi", LocalDate.of(1990, 5, 15), "Verona");
+    Studente s1 = new Studente("Luigi", "Bianchi", LocalDate.of(1998, 9, 20), "Milano", 12345, 2018, "Informatica");
+    StudenteLavoratore sl1 = new StudenteLavoratore("Anna", "Verdi", LocalDate.of(1995, 3, 10), "Roma", 54321, 2015, "Ingegneria", "Acme Inc.", 2500.0);
+    StudenteFuoriSede sfs1 = new StudenteFuoriSede("Carlo", "Neri", LocalDate.of(2000, 11, 5), "Napoli", 67890, 2019, "Biologia", 450.0, 3000.0);
+
+    System.out.println(p1);
+    System.out.println(s1);
+    System.out.println(sl1);
+    System.out.println(sfs1);
+
+    System.out.println("Età di " + p1.getNome() + ": " + p1.calcolaEta());
+    System.out.println("Anni di iscrizione di " + s1.getNome() + ": " + s1.calcolaAnniIscrizione());
+    System.out.println("Reddito annuale di " + sl1.getNome() + ": " + sl1.calcolaRedditoAnnuale());
+    System.out.println("Spese annuali di " + sfs1.getNome() + ": " + sfs1.calcolaSpeseAnnuali());
+
+    System.out.println(s1.isStudenteRegolare());
+    System.out.println(StudenteUniversitario.isStudenteLavoratore(sl1));
+    System.out.println(Persona.confrontaEta(p1, s1));
+    System.out.println(p1.haLoStessoCognome(sl1));
+    System.out.println(s1.isImmatricolatoInCorso("Informatica"));
+    System.out.println(Studente.calcolaTassaMedia(List.of(s1, sfs1)));
+}
 }
