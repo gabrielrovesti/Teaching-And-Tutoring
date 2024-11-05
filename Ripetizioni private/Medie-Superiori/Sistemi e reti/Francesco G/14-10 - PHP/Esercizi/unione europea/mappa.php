89429584578d0f4d1de333c91d779c007d1c2adf
Gabriel Rovesti <rovestigabriel@gmail.com>
2024-11-05 19:15:19 +0100

    Tanta roba oggi

--- Ripetizioni private/Medie-Superiori/Sistemi e reti/Francesco G/14-10 - PHP/Esercizi/unione europea/mappa.php
+++
@@ -1,8 +0,0 @@
-<?php
-    echo"
-        <img src=\"Immagini/Europa.gif\" alt=\"mappa\" usemap=\"\"/>
-        <map=\"\">
-            <area shape=\"rect\" coords=\"350,530,380,540\" href=\"index.php?s=ita\" />
-        </map>
-    "; 
-?>
